ae5ff4872faee8e4ea93763ded129a35960e52f0
GinMu <gin.musheng@gmail.com>
2020-03-14 22:52:52 +0800

    修改映射脚本

---
+++ src/huobi2weidex.js
@@ -0,0 +1,89 @@
+const ccxt = require("./ccxt");
+const config = require("./config");
+const amountLimit = config.amountLimit;
+
+const weidex = new ccxt["weidex"]({
+  address: config.jingtumHuobi.address,
+  secret: config.jingtumHuobi.secret,
+  enableRateLimit: true
+});
+
+const huobipro = new ccxt["huobipro"]({
+  apiKey: config.huobi.access_key,
+  secret: config.huobi.secretkey,
+  verbose: false,
+  timeout: 60000,
+  enableRateLimit: true,
+  urls: {
+    api: {
+      market: config.huobi.market,
+      public: config.huobi.public,
+      private: config.huobi.private,
+      zendesk: config.huobi.zendesk
+    }
+  },
+  hostname: config.huobi.hostname
+});
+
+const cancelWeidexOrders = async (pair) => {
+  try {
+    const orders = await weidex.fetchOpenOrders(pair);
+    for (const order of orders) {
+      try {
+        const res = await weidex.cancelOrder(order.id, pair);
+        console.log("取消成功: ", res);
+      } catch (error) {
+        console.log("取消失败: ", error);
+      }
+    }
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+// 火币订单映射到威链上
+const startMapping = () => {
+  let pairs = config.tradePairs;
+  for (const pair of pairs) {
+    mappingPair(pair);
+  }
+};
+
+const mappingPair = async (pair) => {
+  await cancelWeidexOrders(pair);
+  const order = await huobipro.fetchOrderBook(pair);
+  const { bids, asks } = order;
+
+  const maxAmount = amountLimit[pair].maxAmount;
+  // 映射买单
+  // 等比例缩小100倍，如超过最大数量限制，映射限制的数量
+  for (const bid of bids) {
+    const [price, amount] = bid;
+    weidex
+      .createOrder(pair, "buy", amount / 100 > maxAmount ? maxAmount : amount / 100, price)
+      .then((res) => {
+        console.log("映射成功：", res);
+      })
+      .catch((error) => {
+        console.log("映射失败：", error);
+      });
+  }
+
+  // 映射卖单
+  // 等比例缩小100倍，如超过最大数量限制，映射限制的数量
+  for (const ask of asks) {
+    const [price, amount] = ask;
+    weidex
+      .createOrder(pair, "sell", amount / 100 > maxAmount ? maxAmount : amount / 100, price)
+      .then((res) => {
+        console.log("映射成功：", res);
+      })
+      .catch((error) => {
+        console.log("映射失败：", error);
+      });
+  }
+};
+
+startMapping();
+// 60分钟映射一次
+setInterval(startMapping, 60 * 60 * 1000);
---
+++ src/okex2weidex.js
@@ -0,0 +1,81 @@
+const ccxt = require("./ccxt");
+const config = require("./config");
+const amountLimit = config.amountLimit;
+
+const weidex = new ccxt["weidex"]({
+  address: config.jingtumHuobi.address,
+  secret: config.jingtumHuobi.secret,
+  enableRateLimit: true
+});
+
+const okex3 = new ccxt["okex3"]({
+  apiKey: config.okex.access_key,
+  secret: config.okex.secretkey,
+  verbose: false,
+  timeout: 60000,
+  enableRateLimit: true,
+  password: config.okex.privatekey
+});
+
+const cancelWeidexOrders = async (pair) => {
+  try {
+    const orders = await weidex.fetchOpenOrders(pair);
+    for (const order of orders) {
+      try {
+        const res = await weidex.cancelOrder(order.id, pair);
+        console.log("取消成功: ", res);
+      } catch (error) {
+        console.log("取消失败: ", error);
+      }
+    }
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+// okex订单映射到威链上
+const startMapping = () => {
+  let pairs = config.tradePairs;
+  for (const pair of pairs) {
+    mappingPair(pair);
+  }
+};
+
+const mappingPair = async (pair) => {
+  await cancelWeidexOrders(pair);
+  const order = await okex3.fetchOrderBook(pair);
+  const { bids, asks } = order;
+
+  const maxAmount = amountLimit[pair].maxAmount;
+  // 映射买单
+  // 等比例缩小100倍，如超过最大数量限制，映射限制的数量
+  for (const bid of bids) {
+    const [price, amount] = bid;
+    weidex
+      .createOrder(pair, "buy", amount / 100 > maxAmount ? maxAmount : amount / 100, price)
+      .then((res) => {
+        console.log("映射成功：", res);
+      })
+      .catch((error) => {
+        console.log("映射失败：", error);
+      });
+  }
+
+  // 映射卖单
+  // 等比例缩小100倍，如超过最大数量限制，映射限制的数量
+  for (const ask of asks) {
+    const [price, amount] = ask;
+    weidex
+      .createOrder(pair, "sell", amount / 100 > maxAmount ? maxAmount : amount / 100, price)
+      .then((res) => {
+        console.log("映射成功：", res);
+      })
+      .catch((error) => {
+        console.log("映射失败：", error);
+      });
+  }
+};
+
+startMapping();
+// 60分钟映射一次
+setInterval(startMapping, 60 * 60 * 1000);
