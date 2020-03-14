b1a58f7cdd7f679589c0d02aa4929fc60c422adb
GinMu <gin.musheng@gmail.com>
2020-03-14 21:58:49 +0800

    添加okex_arbitrage.js

--- src/okex_arbitrage.js
+++ src/okex_arbitrage.js
@@ -12,21 +12,13 @@ const weidex = new ccxt["weidex"]({
   enableRateLimit: true
 });
 
-const huobipro = new ccxt["huobipro"]({
-  apiKey: config.huobi.access_key,
-  secret: config.huobi.secretkey,
+const okex3 = new ccxt["okex3"]({
+  apiKey: config.okex.access_key,
+  secret: config.okex.secretkey,
   verbose: false,
   timeout: 60000,
   enableRateLimit: true,
-  urls: {
-    api: {
-      market: config.huobi.market,
-      public: config.huobi.public,
-      private: config.huobi.private,
-      zendesk: config.huobi.zendesk
-    }
-  },
-  hostname: config.huobi.hostname
+  password: config.okex.privatekey
 });
 
 const run = () => {
@@ -38,56 +30,56 @@ const run = () => {
 // bids买单，asks卖单
 const startCheck = async (pair) => {
   try {
-    const markets = await Promise.all([huobipro.fetchOrderBook(pair), weidex.fetchOrderBook(pair)]);
+    const markets = await Promise.all([okex3.fetchOrderBook(pair), weidex.fetchOrderBook(pair)]);
     const [huobiMarkets, weidexMarkets] = markets;
 
-    // 火币最高价买一单
+    // okex最高价买一单
     const maxHuobiBid = huobiMarkets.bids[0];
-    // 火币最低价买一单
+    // okex最低价买一单
     const minHuobiAsk = huobiMarkets.asks[0];
     // 威链最高价买一单
     const maxWeidexBid = weidexMarkets.bids[0];
     // 威链最低价卖一单
     const minWeidexAsk = weidexMarkets.asks[0];
 
-    console.log(`${pair} 火币最高价买单：`, maxHuobiBid);
-    console.log(`${pair} 火币最低价卖单：`, minHuobiAsk);
+    console.log(`${pair} okex最高价买单：`, maxHuobiBid);
+    console.log(`${pair} okex最低价卖单：`, minHuobiAsk);
     console.log(`${pair} 威链最高价买单：`, maxWeidexBid);
     console.log(`${pair} 威链最低价卖单：`, minWeidexAsk);
 
     const maxAmount = amountLimit[pair].maxAmount;
-    // 威链买火币卖
-    // 比较威链最低卖单和火币最高买单
+    // 威链买okex卖
+    // 比较威链最低卖单和okex最高买单
     if (
       new BigNumber(maxHuobiBid[0])
         .div(minWeidexAsk[0])
         .minus(1)
         .isGreaterThanOrEqualTo(profit)
     ) {
-      // 威链买火币卖
-      console.log("威链买火币卖");
+      // 威链买okex卖
+      console.log("威链买okex卖");
       const [weidexPrice, weidexAmount] = minWeidexAsk;
-      const [huobiPrice, huobiAmount] = maxHuobiBid;
-      let amount = weidexAmount > huobiAmount ? huobiAmount : weidexAmount;
+      const [okexPrice, okexAmount] = maxHuobiBid;
+      let amount = weidexAmount > okexAmount ? okexAmount : weidexAmount;
       amount = amount > maxAmount ? maxAmount : amount;
-      await huobipro.createOrder(pair, "limit", "sell", amount, huobiPrice);
+      await okex3.createOrder(pair, "limit", "sell", amount, okexPrice);
       await weidex.createOrder(pair, "buy", amount, weidexPrice);
     }
-    // 威链卖火币买
-    // 比较威链最高买单和火币最低卖单
+    // 威链卖okex买
+    // 比较威链最高买单和okex最低卖单
     else if (
       new BigNumber(maxWeidexBid[0])
         .div(minHuobiAsk[0])
         .minus(1)
         .isGreaterThanOrEqualTo(profit)
     ) {
-      // 威链卖火币买
-      console.log("威链卖火币买");
+      // 威链卖okex买
+      console.log("威链卖okex买");
       const [weidexPrice, weidexAmount] = maxWeidexBid;
-      const [huobiPrice, huobiAmount] = minHuobiAsk;
-      let amount = weidexAmount > huobiAmount ? huobiAmount : weidexAmount;
+      const [okexPrice, okexAmount] = minHuobiAsk;
+      let amount = weidexAmount > okexAmount ? okexAmount : weidexAmount;
       amount = amount > maxAmount ? maxAmount : amount;
-      await huobipro.createOrder(pair, "limit", "buy", amount, huobiPrice);
+      await okex3.createOrder(pair, "limit", "buy", amount, okexPrice);
       await weidex.createOrder(pair, "sell", amount, weidexPrice);
     }
   } catch (error) {
